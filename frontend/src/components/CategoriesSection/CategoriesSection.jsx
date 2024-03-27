2eef493ac552861897b2b0cc0fe6f1d90b27215f
AliDeWeb <alimoradi0business@gmail.com>
2024-03-27 03:38:35 -0700

    categories section imgs changed

--- frontend/src/components/CategoriesSection/CategoriesSection.jsx
+++ frontend/src/components/CategoriesSection/CategoriesSection.jsx
@@ -3,52 +3,60 @@ import React from "react";
 // React Router
 import { Link } from "react-router-dom";
 
-// Icons
-import { IoIosLaptop, IoMdGlasses } from "react-icons/io";
-import { GiClothes, GiWatch } from "react-icons/gi";
-import { IoHome } from "react-icons/io5";
-import { FaBook } from "react-icons/fa6";
+// Imgs
+import categories1 from "../../assets/imgs/categories1.webp";
+import categories2 from "../../assets/imgs/categories2.webp";
+import categories3 from "../../assets/imgs/categories3.webp";
+import categories4 from "../../assets/imgs/categories4.webp";
+import categories5 from "../../assets/imgs/categories5.webp";
+import categories6 from "../../assets/imgs/categories6.webp";
 
 export default function CategoriesSection() {
   return (
     <div className="py-5">
       <div className="container">
         <div className="child:w-1/3 sm:child:w-1/4 md:child:w-1/6 flex font-dana flex-wrap md:flex-nowrap justify-center gap-4 md:gap-0">
-          <div className="md:mx-2 bg-white h-[90px] flex flex-col items-center justify-center hover:bg-slate-200 transition-all rounded-md">
-            <Link className="flex flex-col items-center justify-center">
-              <IoIosLaptop size="1.4rem" />
-              <span className="inline-block mt-2">لپ تاپ</span>
-            </Link>
+          <div>
+            <div className="md:mx-2 bg-white flex flex-col items-center justify-center hover:scale-105 transition-all rounded-md overflow-hidden">
+              <Link className="flex flex-col items-center justify-center">
+                <img src={categories1} alt="categoriesImg" />
+              </Link>
+            </div>
           </div>
-          <div className="md:mx-2 bg-white h-[90px] flex flex-col items-center justify-center hover:bg-slate-200 transition-all rounded-md">
-            <Link className="flex flex-col items-center justify-center">
-              <IoMdGlasses size="1.4rem" />
-              <span className="inline-block mt-2">عینک</span>
-            </Link>
+          <div>
+            <div className="md:mx-2 bg-white flex flex-col items-center justify-center hover:scale-105 transition-all rounded-md overflow-hidden">
+              <Link className="flex flex-col items-center justify-center">
+                <img src={categories2} alt="categoriesImg" />
+              </Link>
+            </div>
           </div>
-          <div className="md:mx-2 bg-white h-[90px] flex flex-col items-center justify-center hover:bg-slate-200 transition-all rounded-md">
-            <Link className="flex flex-col items-center justify-center">
-              <GiClothes size="1.4rem" />
-              <span className="inline-block mt-2">پوشاک</span>
-            </Link>
+          <div>
+            <div className="md:mx-2 bg-white flex flex-col items-center justify-center hover:scale-105 transition-all rounded-md overflow-hidden">
+              <Link className="flex flex-col items-center justify-center">
+                <img src={categories3} alt="categoriesImg" />
+              </Link>
+            </div>
           </div>
-          <div className="md:mx-2 bg-white h-[90px] flex flex-col items-center justify-center hover:bg-slate-200 transition-all rounded-md">
-            <Link className="flex flex-col items-center justify-center">
-              <GiWatch size="1.4rem" />
-              <span className="inline-block mt-2">ساعت</span>
-            </Link>
+          <div>
+            <div className="md:mx-2 bg-white flex flex-col items-center justify-center hover:scale-105 transition-all rounded-md overflow-hidden">
+              <Link className="flex flex-col items-center justify-center">
+                <img src={categories4} alt="categoriesImg" />
+              </Link>
+            </div>
           </div>
-          <div className="md:mx-2 bg-white h-[90px] flex flex-col items-center justify-center hover:bg-slate-200 transition-all rounded-md">
-            <Link className="flex flex-col items-center justify-center">
-              <IoHome size="1.1rem" />
-              <span className="inline-block mt-2">لوازم خانگی</span>
-            </Link>
+          <div>
+            <div className="md:mx-2 bg-white flex flex-col items-center justify-center hover:scale-105 transition-all rounded-md overflow-hidden">
+              <Link className="flex flex-col items-center justify-center">
+                <img src={categories5} alt="categoriesImg" />
+              </Link>
+            </div>
           </div>
-          <div className="md:mx-2 bg-white h-[90px] flex flex-col items-center justify-center hover:bg-slate-200 transition-all rounded-md">
-            <Link className="flex flex-col items-center justify-center">
-              <FaBook size="1.1rem" />
-              <span className="inline-block mt-2">کتاب</span>
-            </Link>
+          <div>
+            <div className="md:mx-2 bg-white flex flex-col items-center justify-center hover:scale-105 transition-all rounded-md overflow-hidden">
+              <Link className="flex flex-col items-center justify-center">
+                <img src={categories6} alt="categoriesImg" />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
