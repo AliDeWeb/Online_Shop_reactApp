56b649c683e3ae395ec7c38d0e060a24a8a3c259
AliDeWeb <alimoradi0business@gmail.com>
2024-03-27 07:15:11 -0700

    some bugs fixed

--- frontend/src/components/TopBrandsSection/TopBrandsSection.jsx
+++ frontend/src/components/TopBrandsSection/TopBrandsSection.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 import brands1 from "../../assets/imgs/brands1.webp";
 
+// React Router
+import { Link } from "react-router-dom";
+
 export default function TopBrandsSection() {
   return (
     <div>
       <div className="overflow-hidden rounded-lg w-40">
-        <img src={brands1} alt="brandsImg" />
+        <Link>
+          <img src={brands1} alt="brandsImg" />
+        </Link>
       </div>
     </div>
   );
