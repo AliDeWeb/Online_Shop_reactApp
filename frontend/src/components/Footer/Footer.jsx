05994c6551c510db168dec40e3e3c309efb50498
AliDeWeb <alimoradi0business@gmail.com>
2024-03-27 06:49:30 -0700

    footer colors changed

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -22,7 +22,7 @@ export default function Footer() {
       <div>
         <div>
           <div className="container">
-            <div className="flex items-center justify-evenly flex-wrap lg:flex-nowrap child:w-1/4 gap-6 lg:gap-0 lg:child:w-auto text-[#494949]">
+            <div className="flex items-center justify-evenly flex-wrap lg:flex-nowrap child:w-1/4 gap-6 lg:gap-0 lg:child:w-auto text-zinc-700">
               <div className="flex flex-col justify-center items-center gap-3 font-dana">
                 <div className="w-[44px]">
                   <img src={shoppingBasket} alt="icon" />
@@ -93,10 +93,10 @@ export default function Footer() {
           <div className="container">
             <div className="flex sm:flex-nowrap flex-wrap child:w-full sm:child:w-1/2 sm:gap-0 gap-5">
               <div className="sm:pl-5">
-                <h3 className="font-danaBold lg:text-lg text-[#494949]">
+                <h3 className="font-danaBold lg:text-lg text-zinc-700">
                   با تیمچه ، آنلاین و ارزان خرید کنید
                 </h3>
-                <p className="font-dana mt-2.5 lg:text-base text-sm lg:line-clamp-none line-clamp-5 text-[#727272]">
+                <p className="font-dana mt-2.5 lg:text-base text-sm lg:line-clamp-none line-clamp-5 text-gray-400">
                   لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و
                   با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و
                   مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
@@ -109,13 +109,13 @@ export default function Footer() {
                 </p>
               </div>
               <div className="sm:pr-5">
-                <h3 className="font-danaBold lg:text-lg text-[#494949]">
+                <h3 className="font-danaBold lg:text-lg text-zinc-700">
                   تماس با ما
                 </h3>
 
-                <ul className="text-[#727272] mt-2.5 font-dana lg:text-base text-sm">
+                <ul className="text-gray-400 mt-2.5 font-dana lg:text-base text-sm">
                   <li className="flex items-center mb-1.5">
-                    <MdEmail color="#727272" className="ml-1.5" size="1rem" />
+                    <MdEmail className="ml-1.5" size="1rem" />
                     ایمیل:
                     <a
                       className="mr-3"
@@ -125,18 +125,14 @@ export default function Footer() {
                     </a>
                   </li>
                   <li className="flex items-center mb-1.5">
-                    <CiMobile3 color="#727272" className="ml-1.5" size="1rem" />
+                    <CiMobile3 className="ml-1.5" size="1rem" />
                     تلفن:
                     <a className="mr-3" href="tel:+989123456789">
                       09123456789
                     </a>
                   </li>
                   <li className="flex items-center">
-                    <FaLocationDot
-                      color="#727272"
-                      className="ml-1.5"
-                      size="1rem"
-                    />
+                    <FaLocationDot className="ml-1.5" size="1rem" />
                     آدرس:
                     <span className="mr-3">
                       تهران ، پاستور ، خیابان آوینی ، پلاک 11
@@ -152,22 +148,22 @@ export default function Footer() {
           <div className="container">
             <div className="flex lg:items-start lg:justify-start flex-wrap lg:flex-nowrap child:w-full gap-4 lg:gap-0 lg:child:w-1/2">
               <div>
-                <h3 className="font-danaBold lg:text-lg text-[#494949]">
+                <h3 className="font-danaBold lg:text-lg text-zinc-700">
                   خبرنامه فروشگاه تیمچه
                 </h3>
-                <div className="mt-4 border-2 border-[#f4f6f8] border-solid rounded-md py-1.5 px-4 w-full lg:w-max">
+                <div className="mt-4 border-2 border-gray-100 border-solid rounded-md py-1.5 px-4 w-full lg:w-max">
                   <input
                     type="text"
                     placeholder="آدرس ایمیل"
-                    className="border-none outline-none font-dana bg-transparent px-2.5 w-[calc(100%-(68.77px))] lg:w-[350px]"
+                    className="text-zinc-700 border-none outline-none font-poppins text-lg bg-transparent px-2.5 w-[calc(100%-(68.77px))] lg:w-[350px]"
                   />
-                  <button className="font-dana bg-[#f4f6f8] py-1.5 px-2 rounded-md text-sm transition-all hover:text-white hover:bg-[#673ab7]">
+                  <button className="text-zinc-700 font-dana bg-gray-100 py-1.5 px-2 rounded-md text-sm">
                     عضوم کن
                   </button>
                 </div>
               </div>
               <div>
-                <h3 className="font-danaBold lg:text-lg text-[#494949]">
+                <h3 className="font-danaBold lg:text-lg text-zinc-700">
                   دانلود اپلیکیشن تیمچه
                 </h3>
                 <div className="mt-4 lg:w-max flex items-center justify-between lg:justify-start lg:gap-6 w-full">
@@ -200,11 +196,11 @@ export default function Footer() {
 
         <div className="py-5 bg-[#e3e3eb]">
           <p
-            className="flex items-center justify-center font-poppins text-sm text-[#494949]"
+            className="flex items-center justify-center font-poppins text-sm text-zinc-700"
             style={{ direction: `ltr` }}
           >
             &copy; <a href="https://www.github.com/AliDeWeb">AliDeWeb</a>,
-            <a href="https://www.github.com/mahdidesignerrr" className="ml-2">
+            <a href="https://www.mahdidesigner.pw" className="ml-2">
               Mahdi
             </a>
           </p>
