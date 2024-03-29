17e92e052f2616fef0acd105704a10df8e28ba19
AliDeWeb <alimoradi0business@gmail.com>
2024-03-28 23:03:45 -0700

    page 404 some bugs fixed

--- frontend/src/pages/Page404/Page404.jsx
+++ frontend/src/pages/Page404/Page404.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
 // React Router
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 // Components
 import { Header, Footer } from "../../configs/Layout/Layout";
@@ -17,7 +17,7 @@ export default function Page404() {
   useEffect(() => {
     setTimeout(() => {
       navigator(`/home`);
-    }, 3000);
+    }, 6000);
   }, []);
 
   return (
@@ -26,10 +26,16 @@ export default function Page404() {
       <div className="container">
         <div className="flex flex-col gap-4 items-center justify-center min-h-[100dvh]">
           <h1 className="font-danaBold text-2xl">صفحه‌ای یافت نشد!</h1>
-          <p className="text-gray-400 text-center">
+          <p className="text-gray-400 text-center font-dana">
             صفحه مورد نظر شما یافت نشد. ممکن است از ابتدا حذف شده باشد ، تغییر
             نام داده شود یا وجود نداشته باشد.
           </p>
+          <Link
+            to="/home"
+            className="font-dana text-gray-400 border border-solid border-orange-300 py-0.5 px-2 rounded-lg transition-all hover:bg-orange-100"
+          >
+            صفحه اصلی
+          </Link>
           <div className="mt-3 lg:w-auto md:w-[550px] sm:w-[420px] w-[320px]">
             <img src={gif404} alt="gif" />
           </div>
