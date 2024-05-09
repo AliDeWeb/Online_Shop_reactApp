ec4b01f918e2fcca1e39d58af8a0acfe669af5b5
AliDeWeb <alimoradi0business@gmail.com>
2024-05-08 21:35:12 -0700

    group selection copleted

--- frontend/src/pages/AdminPanelPages/AdminPanelUsers.jsx
+++ frontend/src/pages/AdminPanelPages/AdminPanelUsers.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Mui
 import { DataGrid } from "@mui/x-data-grid";
@@ -26,7 +26,15 @@ const tableHead = [
     description: "این ستون قابل مرتب سازی نیست.",
     sortable: false,
   },
+  {
+    field: "totalProfit",
+    headerName: "مبالغ خرید",
+    width: 150,
+  },
 ];
+
+// React Spinners
+import BeatLoader from "react-spinners/BeatLoader";
 
 // Axios
 import { getAdminPanelUsers } from "../../configs/axios/axiosConfigs";
@@ -40,129 +48,10 @@ import useUserToken from "../../hooks/useUserToken/useUserToken";
 export default function AdminPanelUsers() {
   const { userToken } = useUserToken();
 
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      firstName: "علی",
-      lastName: "مرادی",
-      email: "alimoradi0@gmail.com",
-      phone: "09125698569",
-    },
-    {
-      id: 2,
-      firstName: "سارا",
-      lastName: "احمدی",
-      email: "saraahmadi1@gmail.com",
-      phone: "09378456234",
-    },
-    {
-      id: 3,
-      firstName: "محمد",
-      lastName: "جعفری",
-      email: "mohammadjafari2@gmail.com",
-      phone: "09101234567",
-    },
-    {
-      id: 4,
-      firstName: "مینا",
-      lastName: "رضایی",
-      email: "minarazaei3@gmail.com",
-      phone: "09198765432",
-    },
-    {
-      id: 5,
-      firstName: "حسین",
-      lastName: "علیزاده",
-      email: "hosein_alizadeh4@gmail.com",
-      phone: "09123456789",
-    },
-    {
-      id: 6,
-      firstName: "زهرا",
-      lastName: "محمدی",
-      email: "zahramohammadi5@gmail.com",
-      phone: "09387654321",
-    },
-    {
-      id: 7,
-      firstName: "امیر",
-      lastName: "تقی زاده",
-      email: "amirtaghzade6@gmail.com",
-      phone: "09176543210",
-    },
-    {
-      id: 8,
-      firstName: "لیلا",
-      lastName: "حسینی",
-      email: "leilahosseini7@gmail.com",
-      phone: "09165432109",
-    },
-    {
-      id: 9,
-      firstName: "اشکان",
-      lastName: "موسوی",
-      email: "ashkanmousavi8@gmail.com",
-      phone: "09154321098",
-    },
-    {
-      id: 10,
-      firstName: "نرگس",
-      lastName: "صالحی",
-      email: "narges_salehi9@gmail.com",
-      phone: "09143210987",
-    },
-    {
-      id: 11,
-      firstName: "آیدا",
-      lastName: "نظری",
-      email: "aida_nazari10@gmail.com",
-      phone: "09132109876",
-    },
-    {
-      id: 12,
-      firstName: "سجاد",
-      lastName: "بهرامی",
-      email: "sajadbahrami11@gmail.com",
-      phone: "09121098765",
-    },
-    {
-      id: 13,
-      firstName: "مینو",
-      lastName: "ابراهیمی",
-      email: "minooebrahimi12@gmail.com",
-      phone: "09110987654",
-    },
-    {
-      id: 14,
-      firstName: "فرهاد",
-      lastName: "رضوانی",
-      email: "farhadrezaee13@gmail.com",
-      phone: "09109876543",
-    },
-    {
-      id: 15,
-      firstName: "مریم",
-      lastName: "خانی",
-      email: "maryamkhani14@gmail.com",
-      phone: "09098765432",
-    },
-    {
-      id: 16,
-      firstName: "علیرضا",
-      lastName: "حسنی",
-      email: "alireza_hosseini15@gmail.com",
-      phone: "09087654321",
-    },
-    {
-      id: 17,
-      firstName: "نازنین",
-      lastName: "احمدیان",
-      email: "nazanin_ahmadiyan16@gmail.com",
-      phone: "09076543210",
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+  const [selectedCells, setSelectedCells] = useState([]);
 
-  const { data, isLoading, refetch } = useQuery(
+  const { isLoading } = useQuery(
     `adminPanelUsers`,
     async () => {
       const res = await getAdminPanelUsers({
@@ -170,13 +59,20 @@ export default function AdminPanelUsers() {
           Authorization: `Bearer ${userToken}`,
         },
       });
-      console.log(res.data);
+      setUsers(() =>
+        res.data.map((el, index) => ({
+          ...el,
+          id: index + 1,
+          totalProfit: el.totalProfit ? el.totalProfit.toLocaleString() : `-`,
+        }))
+      );
       return res.data;
     },
     {
       cacheTime: 30000,
       staleTime: 0,
       refetchOnMount: true,
+      refetchOnWindowFocus: true,
     }
   );
 
@@ -190,19 +86,38 @@ export default function AdminPanelUsers() {
             </h2>
           </div>
         </div>
-        <div style={{ height: 400, width: "100%" }}>
-          <DataGrid
-            rows={users}
-            columns={tableHead}
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: 5 },
-              },
-            }}
-            
-            pageSizeOptions={[5, 10, 20]}
-          />
-        </div>
+        {!isLoading ? (
+          <div style={{ height: 600, width: "100%" }}>
+            <DataGrid
+              rows={users}
+              columns={tableHead}
+              initialState={{
+                pagination: {
+                  paginationModel: { page: 0, pageSize: 12 },
+                },
+              }}
+              pageSizeOptions={[12, 20, 25]}
+              checkboxSelection
+              onRowSelectionModelChange={(cell) => {
+                setSelectedCells([]);
+                cell.forEach((el) => {
+                  setSelectedCells((priv) => {
+                    return [
+                      ...priv,
+                      users.filter((user) => {
+                        return el === user.id;
+                      }),
+                    ];
+                  });
+                });
+              }}
+            />
+          </div>
+        ) : (
+          <div className="flex items-center justify-center w-full h-[200px] text-teal-600">
+            <BeatLoader color="rgb(13, 148, 136)" />
+          </div>
+        )}
       </div>
     </div>
   );
