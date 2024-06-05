0b18bb01a0a9f8a78aa9d6775539f6f7cd6fcb4b
AliDeWeb <alimoradi0business@gmail.com>
2024-06-05 04:38:20 -0700

    increase wallet modal changed

--- frontend/src/components/PanelsSideBar/UserPanelSideBar.jsx
+++ frontend/src/components/PanelsSideBar/UserPanelSideBar.jsx
@@ -1,5 +1,10 @@
+import { useState, useRef } from "React";
+
 // React Router
 import { Link, NavLink, useNavigate } from "react-router-dom";
+
+// React Spinners
+import ClipLoader from "react-spinners/ClipLoader.js";
 
 // icons
 import { CiEdit } from "react-icons/ci";
@@ -7,7 +12,11 @@ import {
   MdOutlineKeyboardArrowLeft,
   MdOutlineShoppingBag,
 } from "react-icons/md";
-import { IoExitOutline, IoHomeOutline } from "react-icons/io5";
+import {
+  IoExitOutline,
+  IoHomeOutline,
+  IoCloseCircleOutline,
+} from "react-icons/io5";
 import { FaRegComment, FaRegHeart } from "react-icons/fa";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { IoMdNotificationsOutline } from "react-icons/io";
@@ -28,10 +37,19 @@ import { useQuery, useQueryClient } from "react-query";
 // Hooks
 import useUserToken from "../../hooks/useUserToken/useUserToken";
 
+// Components
+import { Modal } from "../../configs/Layout/Layout";
+
 export default function UserPanelSideBar() {
+  const [isWalletModalOpen, setIsWalletModalOpen] = useState(false);
+
   const showSwal = withReactContent(Swal);
   const { userToken } = useUserToken();
   const queryClient = useQueryClient();
+
+  const [isDataFetching, setIsDataFetching] = useState(false);
+
+  const payment = useRef(0);
 
   const navigator = useNavigate();
 
@@ -56,7 +74,7 @@ export default function UserPanelSideBar() {
 
         return data;
       },
-    },
+    }
   );
 
   return (
@@ -78,34 +96,7 @@ export default function UserPanelSideBar() {
             <span className="text-zinc-700">کیف پول</span>
             <button
               onClick={() => {
-                showSwal
-                  .fire({
-                    title: "افزایش موجودی",
-                    input: "range",
-                    inputAttributes: {
-                      min: "50000",
-                      max: "50000000",
-                      step: "50000",
-                    },
-                    inputValue: 200000,
-                    confirmButtonText: "تایید",
-                    cancelButtonText: "انصراف",
-                    showCancelButton: true,
-                    showCloseButton: true,
-                  })
-                  .then((res) => {
-                    if (res.isConfirmed) {
-                      updateWallet({
-                        url: `/${res.value}`,
-                        headers: {
-                          Authorization: `Bearer ${userToken}`,
-                        },
-                      }).then((res) => {
-                        window.location.replace(res.data.url);
-                        refetch();
-                      });
-                    }
-                  });
+                setIsWalletModalOpen(true);
               }}
               className="text-sm flex items-center text-teal-600 transition-all hover:scale-95 line-clamp-1"
             >
@@ -114,6 +105,59 @@ export default function UserPanelSideBar() {
                 <MdOutlineKeyboardArrowLeft />
               </span>
             </button>
+            <Modal
+              isOpen={isWalletModalOpen}
+              changeVisibility={setIsWalletModalOpen}
+            >
+              <div className="flex items-center gap-2 mr-2 sm:mr-6 mt-4 text-zinc-700">
+                <button
+                  onClick={() => {
+                    setIsWalletModalOpen(false);
+                  }}
+                >
+                  <IoCloseCircleOutline size="1.5rem" />
+                </button>
+                <h2 className="font-danaDemi md:font-danaBold md:text-lg line-clamp-1">
+                  افزایش موجودی
+                </h2>
+              </div>
+              <div className="flex flex-col py-2 px-4 rounded-lg text-zinc-700 mt-4 font-dana">
+                <input
+                  id="commentDisc"
+                  type="text"
+                  placeholder="مبلغ مورد نظر خود را وارد نمایید"
+                  className="font-dana mb-4  mt-1 outline-none bg-transparent border-b border-solid border-gray-300 focus:border-orange-300 pb-2 text-sm"
+                  onInput={(e) => {
+                    payment.current = e.target.value;
+                  }}
+                />
+              </div>
+              <button
+                className="font-danaBold mt-4 cursor-pointer w-full h-[40px] bg-orange-200 hover:bg-orange-300/80 transition-all rounded-lg flex justify-center items-center text-zinc-700"
+                type="submit"
+                onClick={() => {
+                  setIsDataFetching(true);
+
+                  updateWallet({
+                    url: `/${payment.current}`,
+                    headers: {
+                      Authorization: `Bearer ${userToken}`,
+                    },
+                  }).then((res) => {
+                    console.log(res);
+                    window.location.replace(res.data.url);
+                    setIsDataFetching(false);
+                    refetch();
+                  });
+                }}
+              >
+                {isDataFetching ? (
+                  <ClipLoader color="#d97706" size="18" />
+                ) : (
+                  "ادامه ..."
+                )}
+              </button>
+            </Modal>
           </div>
           <span className="text-teal-600 font-danaBold text-sm line-clamp-1">
             {!isLoading && (data?.user?.wallet).toLocaleString()} تومان
