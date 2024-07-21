99399ee62465709413ee67f0b5e465e91cbcb430
MahdiDesigner <mmbaniensan@gmail.com>
2024-07-21 11:26:53 +0330

    improve animations

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,24 +1,25 @@
 import { createPortal } from "react-dom";
-import {Motion} from "../../configs/Layout/Layout";
+import { Motion } from "../../configs/Layout/Layout";
 
 export default function Modal({ isOpen, children, changeVisibility }) {
-   return createPortal(
-      <Motion>
-         {isOpen && (
-            <Motion.opacity>
-               <div
-                  className={` modal-wrapper fixed bottom-0 top-0 right-0 left-0 w-full h-[100dvh] bg-black/40 z-30 flex items-center justify-center transition-all`}
-               >
-                  <div
-                     style={{ boxShadow: `0 0 20px rgb(48,45,45)` }}
-                     className="w-[350px] sm:w-[450px] md:w-[650px] py-2 px-4 rounded-lg border-2 border-solid border-orange-300 bg-[#ececec]"
-                  >
-                     {children}
-                  </div>
-               </div>
-            </Motion.opacity>
-         )}
-      </Motion>,
-      document.body
-   );
+  return createPortal(
+    <Motion>
+      {isOpen && (
+        <Motion.opacity>
+          <div
+            className="modal-wrapper fixed bottom-0 top-0 right-0 left-0 w-full h-[100dvh] bg-black/40 z-30 flex items-center justify-center transition-all"
+          >
+            <Motion.page
+              onClick={(e) => e.stopPropagation()}
+              style={{ boxShadow: "0 0 20px rgb(48,45,45)" }}
+              className="w-[350px] sm:w-[450px] md:w-[650px] py-2 px-4 rounded-lg border-2 border-solid border-orange-300 bg-[#ececec]"
+            >
+              {children}
+            </Motion.page>
+          </div>
+        </Motion.opacity>
+      )}
+    </Motion>,
+    document.body
+  );
 }
